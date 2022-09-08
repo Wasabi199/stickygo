3def8c83e9f559f49160db64aac626db77d59891
Wasabi199 <xxryan0909@gmail.com>
2022-09-08 22:52:19 +0800

    change font

--- sticky-go/tailwind.config.js
+++ sticky-go/tailwind.config.js
@@ -13,7 +13,8 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Poppins', 'sans-serif', ...defaultTheme.fontFamily.sans],
+                // 'poppins': ['Poppins', 'sans-serif'] 
             },
         },
     },
